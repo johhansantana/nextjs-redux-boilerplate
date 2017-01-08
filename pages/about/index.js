c72996a0dddcebd5fff3cf88b7800c67633cbbd2
Johhan Santana <johhan.santana@gmail.com>
2017-01-07 22:24:49 -0400

    rename pages folder and layout component to fix case sensitive bug when deployed with now

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from '../../components/layout'
+class About extends Component {
+  render() {
+    return (
+      <Layout title="About us">
+        <p>about us</p>
+      </Layout>
+    )
+  }
+}
+export default About;
